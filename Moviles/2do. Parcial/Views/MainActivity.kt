dbf2e78d21e6a6ccaa751752cf71f49cb22e00a4
Francisco Javier Bueno Vasquez <francisco.j.bueno@oracle.com>
2024-04-17 15:02:18 -0600

    2do_Parcial_Views
    2do_Parcial_Views

--- Moviles/2do. Parcial/Views/MainActivity.kt
+++ Moviles/2do. Parcial/Views/MainActivity.kt
@@ -1,22 +1,46 @@
-package com.example.p_movil_viernes
+package com.example.p_movil_miercoles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Button
+import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnAccept: Button
-    private lateinit var text_1: TextView
+    //private lateinit var btnAccept: Button
+    //private lateinit var textview1: TextView
+    private lateinit var editText: EditText
+    private lateinit var editText2: EditText
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /*btnAccept = findViewById(R.id.btnAccept)
-        text_1 = findViewById(R.id.text_1)
+        textview1 = findViewById(R.id.text_view1)
         btnAccept.setOnClickListener {
-            text_1.text = getString(R.string.button_clicked)
+            textview1.text = getString(R.string.button_clicked)
         }*/
+        textView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
+        editText2 = findViewById(R.id.editText2)
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                textView.text = editText.text
+            }
+        })
     }
 }
